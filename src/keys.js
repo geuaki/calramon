db9556653d7d6701ec45cd04ef18cf11f4264446
ramon <ramon.torras.sds@gmail.com>
2021-05-15 21:06:16 +0200

    ultimos retoques

--- src/keys.js
+++ src/keys.js
@@ -2,4 +2,6 @@ module.exports = {
     mongodb: {
         URI: 'mongodb+srv://ramon:ramon123@cluster0.xjl8w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     }
+    //mongodb+srv://ramon:ramon123@cluster0.xjl8w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+    //mongodb://localhost:27017/tiendaonline
 };
